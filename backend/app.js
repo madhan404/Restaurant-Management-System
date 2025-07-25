cb3918575ef4ebc3d6e264dbe5f194f213be5d1c
Madhaneey <madhanrajs047@gmail.com>
2025-07-25 21:47:34 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -22,10 +22,27 @@ const io = new Server(server, {
 });
 
 // Middleware
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }));
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://restaurant-managementsystem.netlify.app'
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
+
 app.use(express.json());
 
 // Make io accessible to routes
